8fc752dd026ddffd29a5dc7ad9d48f6552994cfd
Allen Liu <mrallenliu@gmail.com>
2018-12-10 19:39:08 +0000

    New Socket
    New Socket Program

---
+++ socket2/client2.py
@@ -0,0 +1,22 @@
+import socket
+
+HOST = 'localhost'
+PORT = 12341
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+s.connect((HOST, PORT))
+
+print("Enter 'q' to quit")
+
+while True:
+
+    mystring = input("Type your message: ")
+    b = bytes(mystring, 'utf-8')
+    s.sendall(b)
+
+    if mystring == 'q':
+        print("Connection terminated.")
+        break
+
+s.close()
---
+++ socket2/type.py
@@ -0,0 +1,3 @@
+string = "Allen"
+
+print(type(string))
